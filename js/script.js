39616d6914f7698aa5e8abf6adc46217226a40d4
buloko <buloko8@gmail.com>
2023-06-10 13:56:02 -0400

    adding a styling for a single img page and buttons

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Get the image elements
+const images = document.querySelectorAll('.gallery-image');
+
+// Add click event listener to each image
+images.forEach(image => {
+  image.addEventListener('click', showSingleImage);
+});
+
+// Function to show a single image
+function showSingleImage(event) {
+  // Get the clicked image source
+  const clickedImageSrc = event.target.src;
+
+  // Create a new image element
+  const singleImage = document.createElement('img');
+  singleImage.src = clickedImageSrc;
+
+  // Clear the existing images in the gallery
+  const galleryContainer = document.querySelector('.gallery-container');
+  galleryContainer.innerHTML = '';
+
+  // Append the single image to the gallery container
+  galleryContainer.appendChild(singleImage);
+}
