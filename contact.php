11c00b659ceb850ad65cf15630e3336e77b1c142
buloko <buloko8@gmail.com>
2023-06-14 11:04:46 -0400

    switch home to index, add php mail code

---
+++ contact.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $contact = $_POST["contact"];
+  $message = $_POST["message"];
+
+  $to = "q2murphy@gmai.com"; // Replace with your email address
+  $subject = "New Contact Form Submission";
+  $body = "Name: $name\nEmail: $email\nContact: $contact\nMessage: $message";
+
+  $headers = "From: $email";
+
+  if (mail($to, $subject, $body, $headers)) {
+    echo "Thank you for your message. I will get back to you soon!";
+  } else {
+    echo "Oops! Something went wrong. Please try again.";
+  }
+}
+?>
