660e8c975e743182181e2527550b4b36f3034176
buloko <buloko8@gmail.com>
2023-06-14 18:18:34 -0400

    added node to ignore, dload tailwind, syntax error

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    purge: [
+      './index.html',
+      './pages/bio.html',
+      './pages/contact.html',
+      '.css/bio.css',
+      '.css/contact.css',
+      '.css/style.css',
+      
+      // Add more HTML files if needed
+    ],
+    // Rest of the configuration...
+  };
